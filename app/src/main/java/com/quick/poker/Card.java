0281bf2afc7cbd685401899a07f5232abc81a017
quick <quick.wang@gmail.com>
2018-12-11 00:20:23 +0800

    52 cards

--- app/src/main/java/com/quick/poker/Card.java
+++ app/src/main/java/com/quick/poker/Card.java
@@ -1,7 +1,7 @@
 package com.quick.poker;
 
 public class Card {
-    static char[] suits = {'c', 'd', 'h', 's'};
+    static char[] suits = {'\u2663', '\u2666', '\u2665', '\u2660'};
     int value;
 
     public Card(int value) {
